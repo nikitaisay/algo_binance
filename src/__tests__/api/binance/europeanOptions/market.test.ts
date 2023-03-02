a5ea7cf5784f9aafd49b0bd92e2838ac20931846
misai-godaddy <misai@godaddy.com>
2023-03-02 21:16:41 +0200

    updated unit tests

--- src/__tests__/api/binance/europeanOptions/market.test.ts
+++ src/__tests__/api/binance/europeanOptions/market.test.ts
@@ -1,119 +1,72 @@
 import { 
   test, 
   describe, 
-  expect
+  expect,
+  beforeEach
 } from "@jest/globals";
 
 import { BinanceEuropeanOptionsMarketApi } from "../../../../api/binance/europeanOptions/market";
 
 describe("Test Binance European Options Market API", () => {
-  const api = new BinanceEuropeanOptionsMarketApi({
-    enableTestnet: true,
-    apiKey: process.env.BINANCE_TESTNET_API_KEY,
-    apiSecret: process.env.BINANCE_TESTNET_API_SECRET,
+  let api: BinanceEuropeanOptionsMarketApi;
+
+  beforeEach(() => {
+    api = new BinanceEuropeanOptionsMarketApi({
+      enableTestnet: true,
+      apiKey: process.env.BINANCE_TESTNET_API_KEY,
+      apiSecret: process.env.BINANCE_TESTNET_API_SECRET,
+    });
   });
 
-  test("Should test connectivity", async () => {
-    try {
+  describe("testConnectivity", () => {
+    test("Should test connectivity", async () => {
       const data = await api.testConnectivity();
       expect(data).toBeDefined();
-    } catch (error) {
-      throw new Error(`Request failed: ${error}`);
-    }
+    });
   });
 
-  // test("Should check server time", async () => {
-  //   try {
+  // describe("checkServerTime", () => {
+  //   test("Should check server time", async () => {
   //     const data = await api.checkServerTime();
   //     expect(data).toBeDefined();
-  //   } catch (error) {
-  //     throw new Error(`Request failed: ${error}`);
-  //   }
+  //   });
   // });
 
-  // test("Should get exchange info", async () => {
-  //   try {
+  // describe("getExchangeInfo", () => {
+  //   test("Should get exchange info", async () => {
   //     const data = await api.getExchangeInfo();
   //     expect(data).toBeDefined();
-  //   } catch (error) {
-  //     throw new Error(`Request failed: ${error}`);
-  //   }
+  //   });
   // });
 
-  // test("Should get option mark price", async () => {
-  //   try {
+  // describe("getOptionMarkPrice", () => {
+  //   test("Should get option mark price", async () => {
   //     const data = await api.getOptionMarkPrice();
   //     expect(data).toBeDefined();
-  //   } catch (error) {
-  //     throw new Error(`Request failed: ${error}`);
-  //   }
+  //   });
   // });
 
-  // test("Should get 24hr ticker price change statistics", async () => {
-  //   try {
+  // describe("get24hrTickerPriceChangeStatistics", () => {
+  //   test("Should get 24hr ticker price change statistics", async () => {
   //     const data = await api.get24hrTickerPriceChangeStatistics();
   //     expect(data).toBeDefined();
-  //   } catch (error) {
-  //     throw new Error(`Request failed: ${error}`);
-  //   }
+  //   });
   // });
 
-  // describe("Should get historical exercise records", () => {
-  //   test("Without params", async () => {
-  //     try {
-  //       const data = await api.getHistoricalExerciseRecords();
-  //       expect(data).toBeDefined();
-  //     } catch (error) {
-  //       throw new Error(`Request failed: ${error}`);
-  //     }
+  // describe("getHistoricalExerciseRecords", () => {
+  //   test("Should get historical exercise records", async () => {
+  //     const data = await api.getHistoricalExerciseRecords();
+  //     expect(data).toBeDefined();
   //   });
 
-  //   test("With startTime", async () => {
-  //     try {
-  //       const data = await api.getHistoricalExerciseRecords({
-  //         startTime: Date.now() - 10000,
-  //       });
-  //       expect(data).toBeDefined();
-  //     } catch (error) {
-  //       throw new Error(`Request failed: ${error}`);
-  //     }
-  //   });
-
-  //   test("With endTime", async () => {
-  //     try {
-  //       const data = await api.getHistoricalExerciseRecords({
-  //         endTime: Date.now(),
-  //       });
-  //       expect(data).toBeDefined();
-  //     } catch (error) {
-  //       throw new Error(`Request failed: ${error}`);
-  //     }
-  //   });
-
-  //   test("With endTime and startTime", async () => {
-  //     try {
-  //       const data = await api.getHistoricalExerciseRecords({
-  //         endTime: Date.now(),
-  //         startTime: Date.now() - 10000,
-  //       });
-  //       expect(data).toBeDefined();
-  //     } catch (error) {
-  //       throw new Error(`Request failed: ${error}`);
-  //     }
-  //   });
-
-  //   if (!api.enableTestnet) {
-  //     test("With limit", async () => {
-  //       try {
-  //         const data = await api.getHistoricalExerciseRecords({
-  //           limit: 1,
-  //         });
-  //         expect(data).toBeDefined();
-  //         expect(data).toHaveLength(1);
-  //       } catch (error) {
-  //         throw new Error(`Request failed: ${error}`);
-  //       }
+  //   test("Should get historical exercise records within a specific date range", async () => {
+  //     const startTime = Date.now();
+  //     const endTime = startTime - 86400000; // 24 hours ago
+  //     const data = await api.getHistoricalExerciseRecords({
+  //       startTime,
+  //       endTime,
   //     });
-  //   }
+  //     expect(data).toBeDefined();
+  //   });
   // });
 });
